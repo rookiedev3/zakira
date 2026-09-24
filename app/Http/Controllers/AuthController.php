58ebedf693036d5f5893aa598213aa22d623d36a
Zahwa Ayu Ramadhani <zahwaayu.stu@pnc.ac.id>
2026-09-24 11:50:38 +0700

    remember me

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -58,7 +58,8 @@ class AuthController extends Controller
 
     public function destroy(Request $request): RedirectResponse
     {
-        Auth::logout();
+        Auth::guard('web')->logout();
+        
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
